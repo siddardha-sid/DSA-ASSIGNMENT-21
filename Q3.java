1f5156ebe889b413f950347b8092ccea875a6c8b
siddardha-sid <67356713+siddardha-sid@users.noreply.github.com>
2023-07-20 15:32:11 +0530

    Add files via upload

---
+++ Q3.java
@@ -0,0 +1,20 @@
+package problems;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+public class Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
